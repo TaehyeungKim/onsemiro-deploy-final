3443d1e178e60a24afdf206dbb3ebb69043ce530
Taehyeungkim <kth1837@snu.ac.kr>
2024-07-19 16:33:29 +0900

    changed self_auth to phone_auth

--- src/routes/SignUp/authentication.jsx
+++ src/routes/SignUp/authentication.jsx
@@ -1,6 +1,6 @@
 import { DataContext } from ".";
 import { useContext } from "react";
-import { FloatingSection, SectionTitle } from "./components";
+import { FloatingSection, SectionTitle, CustomTextInput } from "./components";
 import IconImage from "../../components/IconImage";
 import check from "../../assets/check_black.png";
 import search from "../../assets/search.png";
@@ -11,7 +11,7 @@ export default function AuthenticateSelf() {
   return (
     <>
       <FloatingSection addedStyle="mt-3">
-        <h3>먼저, 본인인증과 학교 인증이 필요해요!</h3>
+        <h3>먼저, 전화 번호 인증과 학교 인증이 필요해요!</h3>
         <h5 className="flex items-center mt-2">
           <div className="w-3">
             <IconImage src={check} />
@@ -20,39 +20,51 @@ export default function AuthenticateSelf() {
         </h5>
       </FloatingSection>
       <FloatingSection addedStyle="mt-3">
-        <SectionTitle>본인인증</SectionTitle>
-        <div className="flex mt-2 items-center">
-          <button
-            className="bg-sub grow rounded-lg py-2"
-            onClick={() => {
-              dataContext?.setter({ ...dataContext.data, auth_self: true });
-              console.log("auth_self");
-            }}
-          >
-            본인인증
-          </button>
-          <div className="w-7 h-full border-2 rounded-full ml-5">
-            <IconImage src={check} />
+        <SectionTitle>번호 인증</SectionTitle>
+        <div className="mt-2">
+          <div className="mb-2">
+            <CustomTextInput
+              placeholder={"이름을 입력하세요."}
+              id={"auth_name"}
+              label={"이름"}
+            />
+          </div>
+          <div className="mb-2 flex">
+            <CustomTextInput id="auth_phone" placeholder={"전화번호"} />
+            <MainCustomButton
+              addedStyle={"h-full py-0 ml-6 flex items-center"}
+              event={{
+                onClick: () => {
+                  // console.log("auth_school");
+                  // dataContext?.setter({
+                  //   ...dataContext.data,
+                  //   auth_school: true,
+                  // });
+                },
+              }}
+            >
+              인증
+            </MainCustomButton>
           </div>
         </div>
       </FloatingSection>
       <FloatingSection addedStyle="mt-20">
         <SectionTitle>학교인증</SectionTitle>
         <div className="mt-2 flex flex-col gap-2">
-          <div className="relative h-8  flex items-center rounded-lg overflow-hidden">
-            <input
-              className="h-full grow placeholder:text-xs placeholder:pl-3 bg-background"
-              placeholder="학교를 검색해주세요"
-            ></input>
+          <div className="relative flex items-center">
+            <CustomTextInput
+              id="auth_school_search"
+              placeholder={"학교를 검색해주세요."}
+            />
             <button className="h-full absolute right-1">
               <IconImage src={search} />
             </button>
           </div>
-          <div className="relative h-8 flex items-center rounded-lg">
-            <input
-              className="h-full grow block placeholder:text-xs placeholder:pl-3 bg-background rounded-lg"
-              placeholder="학교 이메일"
-            ></input>
+          <div className="flex items-center">
+            <CustomTextInput
+              id="auth_schoolEmail"
+              placeholder={"학교 이메일"}
+            />
             <MainCustomButton
               addedStyle={"h-full py-0 ml-6 flex items-center"}
               event={{
@@ -68,11 +80,11 @@ export default function AuthenticateSelf() {
               인증
             </MainCustomButton>
           </div>
-          <div className="relative h-8 flex items-center">
-            <input
-              className="h-full grow block placeholder:text-xs placeholder:pl-3 bg-background rounded-lg"
-              placeholder="인증번호 입력하기"
-            ></input>
+          <div>
+            <CustomTextInput
+              id="auth_school_verify_code"
+              placeholder={"인증번호 입력하기"}
+            />
           </div>
         </div>
       </FloatingSection>
