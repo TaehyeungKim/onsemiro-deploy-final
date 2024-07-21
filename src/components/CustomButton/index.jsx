931cc23a2c9b817428465af3ffa1ac91ffccf99d
Taehyeungkim <kth1837@snu.ac.kr>
2024-07-22 01:49:39 +0900

    :sparkles: added custom count button

--- src/components/CustomButton/index.jsx
+++ src/components/CustomButton/index.jsx
@@ -8,3 +8,21 @@ export function MainCustomButton({ addedStyle = "", event = null, children }) {
     </button>
   );
 }
+
+export function CustomButtonWithCount({
+  count,
+  addedStyle = "",
+  event = null,
+  children,
+}) {
+  return (
+    <div className="w-full relative">
+      <div className="absolute w-5 rounded-full aspect-square bg-count-red text-white -top-2 -right-2 flex items-center justify-center text-xs">
+        {count}
+      </div>
+      <MainCustomButton addedStyle={`${addedStyle} !w-full`} event={event}>
+        {children}
+      </MainCustomButton>
+    </div>
+  );
+}
