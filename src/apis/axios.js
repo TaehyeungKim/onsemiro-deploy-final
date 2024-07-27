e8cd525a48d5dac8d75912bd78c2aad974b7926e
Taehyeungkim <kth1837@snu.ac.kr>
2024-07-27 14:13:40 +0900

    fixing

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -16,6 +16,7 @@ export const instanceWithToken = axios.create();
 instanceWithToken.interceptors.request.use(
   (config) => {
     const accessToken = getCookie("access_token");
+    console.log(accessToken);
 
     if (!accessToken) {
       return;
